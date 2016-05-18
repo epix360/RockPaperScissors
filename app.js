2c17bb193bb2777f0a69984ffe8ededcfbfc7d47
Eric Porter <eporter@scentsy.com>
2016-05-17 20:50:24 -0600

    Followed Tom's guide to encapsulate JS using objects

--- app.js
+++ app.js
@@ -1,54 +1,49 @@
-var items = ['rock', 'paper', 'scissors'];
-var computerItem;
+(function () {
 
-function loadComputerChoice() {
-    var computerChoice = Math.floor(Math.random() * 3);
-    computerItem = items[computerChoice];
-    console.log('Computer Item: ', computerItem);
-}
+    var Game = function () {
 
-window.play = function (playerItem) {
-  
-  //Player = Rock
-  if(playerItem === 'rock' && computerItem === 'rock') {
-      document.getElementById('result').innerHTML = "Computer chose rock. It's a tie.";
-  } 
-  else if(playerItem === 'rock' && computerItem === 'paper') {
-      document.getElementById('result').innerHTML = "Computer chose paper. Computer wins!";
-  } 
-  else if(playerItem === 'rock' && computerItem === 'scissors') {
-      document.getElementById('result').innerHTML = "Computer chose scissors. You win!";
-  } 
-  
-  //Player = Paper
-  if(playerItem === 'paper' && computerItem === 'rock') {
-      document.getElementById('result').innerHTML = "Computer chose rock. You win!";
-  } 
-  else if(playerItem === 'paper' && computerItem === 'paper') {
-      document.getElementById('result').innerHTML = "Computer chose paper. It's a tie.";
-  } 
-  else if(playerItem === 'paper' && computerItem === 'scissors') {
-      document.getElementById('result').innerHTML = "Computer chose scissors. Computer wins!";
-  } 
-  
-  //Player = Scissors
-  if(playerItem === 'scissors' && computerItem === 'rock') {
-      document.getElementById('result').innerHTML = "Computer chose rock. Computer wins!";
-  } 
-  else if(playerItem === 'scissors' && computerItem === 'paper') {
-      document.getElementById('result').innerHTML = "Computer chose paper. You win!";
-  } 
-  else if(playerItem === 'scissors' && computerItem === 'scissors') {
-      document.getElementById('result').innerHTML = "Computer chose scissors. It's a tie.";
-  } 
-};
+        this.items = ['rock', 'paper', 'scissors'];
+        this.computerItem;
+    };
 
-window.restart = function () {
-    loadComputerChoice();
-    document.getElementById('result').innerHTML = "Computer is ready. Make your move.";
-};
+    Game.prototype.start = function () {
+        var computerChoice = Math.floor(Math.random() * 3);
 
-loadComputerChoice();
+        this.computerItem = this.items[computerChoice];
 
+        console.log('Computer Item: ', this.computerItem);
+        document.getElementById('result').innerHTML = 'Computer is ready. Make your move.';
+    };
 
+    Game.prototype.play = function(playerItem) {
 
+        //Player = Rock
+        if (playerItem === 'rock' && this.computerItem === 'rock') {
+            document.getElementById('result').innerHTML = 'Computer chose rock. It\'s a tie.';
+        } else if (playerItem === 'rock' && this.computerItem === 'paper') {
+            document.getElementById('result').innerHTML = 'Computer chose paper. Computer wins!';
+        } else if (playerItem === 'rock' && this.computerItem === 'scissors') {
+            document.getElementById('result').innerHTML = "Computer chose scissors. You win!";
+        }
+
+        //Player = Paper
+        if (playerItem === 'paper' && this.computerItem === 'rock') {
+            document.getElementById('result').innerHTML = 'Computer chose rock. You win!';
+        } else if (playerItem === 'paper' && this.computerItem === 'paper') {
+            document.getElementById('result').innerHTML = 'Computer chose paper. It\'s a tie.';
+        } else if (playerItem === 'paper' && this.computerItem === 'scissors') {
+            document.getElementById('result').innerHTML = 'Computer chose scissors. Computer wins!';
+        }
+
+        //Player = Scissors
+        if (playerItem === 'scissors' && this.computerItem === 'rock') {
+            document.getElementById('result').innerHTML = 'Computer chose rock. Computer wins!';
+        } else if (playerItem === 'scissors' && this.computerItem === 'paper') {
+            document.getElementById('result').innerHTML = 'Computer chose paper. You win!';
+        } else if (playerItem === 'scissors' && this.computerItem === 'scissors') {
+            document.getElementById('result').innerHTML = 'Computer chose scissors. It\'s a tie.';
+        }
+    };
+
+    window.RockPaperScissors = Game;
+})();
